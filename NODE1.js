c5330ab33957e3cc386cb0f75e0ccd6e5206fac5
Fully34 <cfullinw34@gmail.com>
2015-07-20 11:28:11 -0600

    first commit

---
+++ NODE1.js
@@ -0,0 +1,57 @@
+
+//===========================================================================//
+                        /* ~~~ Luminosity ~~~ */ 
+//===========================================================================//
+
+// Write a program that takes a color and returns the luminosity of that color using the following formula: luminosity = 0.2126*r + 0.7152*g + 0.0722*b
+
+// var red = process.argv[2];
+// var green = process.argv[3];
+// var blue = process.argv[4];
+
+// function luminosityCheck(r, g, b) {
+
+//     var luminosity = ( (0.2126 * r) + (0.7152 * g) + (0.0722 * b) );
+
+//     return luminosity;
+// }
+
+// console.log( luminosityCheck(red, green, blue) );
+
+//> VICTORY
+
+// Write a separate program that takes a color and returns 'light' if the luminosity is greater than 155 and 'dark' otherwise. At this point, you can just copy the luminosity function from the last step into a new file. You'll require it as a module in Part III.
+
+// function luminosityNameCheck(r, g, b) {
+
+//     var luminosity = ( (0.2126 * r) + (0.7152 * g) + (0.0722 * b) );
+
+//     if ( luminosity > 155 ) {
+
+//         return 'light';
+
+//     } else {
+
+//         return 'dark';
+//     }
+// }
+
+// console.log( luminosityNameCheck(red, green, blue) );
+
+//>VICTORY
+
+
+//===========================================================================//
+                        /* ~~~ require ~~~ */ 
+//===========================================================================//
+
+
+
+
+
+
+
+
+
+
+
